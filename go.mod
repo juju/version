e881e65657e19e6d1f2ea52fc48c28b2013eb98d
Ian Booth <ian.booth@canonical.com>
2021-03-19 11:47:52 +1000

    Rename Series to Release to make the version Binary more generic

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/juju/version
+module github.com/juju/version/v2
 
 go 1.14
 
--- version_test.go
+++ version_test.go
@@ -12,7 +12,7 @@ import (
 	gc "gopkg.in/check.v1"
 	goyaml "gopkg.in/yaml.v2"
 
-	"github.com/juju/version"
+	"github.com/juju/version/v2"
 )
 
 type suite struct{}
@@ -202,8 +202,8 @@ func binaryVersion(major, minor, patch, build int, tag, series, arch string) ver
 			Build: build,
 			Tag:   tag,
 		},
-		Series: series,
-		Arch:   arch,
+		Release: series,
+		Arch:    arch,
 	}
 }
 
@@ -213,10 +213,10 @@ func (*suite) TestParseBinary(c *gc.C) {
 		err    string
 		expect version.Binary
 	}{{
-		v:      "1.2.3-trusty-amd64",
-		expect: binaryVersion(1, 2, 3, 0, "", "trusty", "amd64"),
+		v:      "1.2.3-ubuntu-amd64",
+		expect: binaryVersion(1, 2, 3, 0, "", "ubuntu", "amd64"),
 	}, {
-		v: "1.2-tag3-bionic-amd64",
+		v: "1.2-tag3-windows-amd64",
 		expect: version.Binary{
 			Number: version.Number{
 				Major: 1,
@@ -225,18 +225,18 @@ func (*suite) TestParseBinary(c *gc.C) {
 				Build: 4,
 				Tag:   "tag",
 			}.ToPatch(),
-			Series: "bionic",
-			Arch:   "amd64",
+			Release: "windows",
+			Arch:    "amd64",
 		},
 	}, {
-		v:      "1.2.3.4-trusty-amd64",
-		expect: binaryVersion(1, 2, 3, 4, "", "trusty", "amd64"),
+		v:      "1.2.3.4-ubuntu-amd64",
+		expect: binaryVersion(1, 2, 3, 4, "", "ubuntu", "amd64"),
 	}, {
-		v:      "1.2-alpha3-trusty-amd64",
-		expect: binaryVersion(1, 2, 3, 0, "alpha", "trusty", "amd64"),
+		v:      "1.2-alpha3-ubuntu-amd64",
+		expect: binaryVersion(1, 2, 3, 0, "alpha", "ubuntu", "amd64"),
 	}, {
-		v:      "1.2-alpha3.4-trusty-amd64",
-		expect: binaryVersion(1, 2, 3, 4, "alpha", "trusty", "amd64"),
+		v:      "1.2-alpha3.4-ubuntu-amd64",
+		expect: binaryVersion(1, 2, 3, 4, "alpha", "ubuntu", "amd64"),
 	}, {
 		v:   "1.2.3",
 		err: "invalid binary version.*",
@@ -247,7 +247,7 @@ func (*suite) TestParseBinary(c *gc.C) {
 		v:   "1.2.3--amd64",
 		err: "invalid binary version.*",
 	}, {
-		v:   "1.2.3-trusty-",
+		v:   "1.2.3-ubuntu-",
 		err: "invalid binary version.*",
 	}}
 
@@ -264,12 +264,12 @@ func (*suite) TestParseBinary(c *gc.C) {
 
 	for i, test := range parseTests {
 		c.Logf("second test, %d: %q", i, test.v)
-		v := test.v + "-trusty-amd64"
+		v := test.v + "-ubuntu-amd64"
 		got, err := version.ParseBinary(v)
 		expect := version.Binary{
-			Number: test.expect,
-			Series: "trusty",
-			Arch:   "amd64",
+			Number:  test.expect,
+			Release: "ubuntu",
+			Arch:    "amd64",
 		}
 		if test.err != "" {
 			c.Assert(err, gc.ErrorMatches, strings.Replace(test.err, "version", "binary version", 1))
@@ -306,7 +306,7 @@ func (*suite) TestBinaryMarshalUnmarshal(c *gc.C) {
 		}
 		// Work around goyaml bug #1096149
 		// SetYAML is not called for non-pointer fields.
-		bp := version.MustParseBinary("1.2.3-trusty-amd64")
+		bp := version.MustParseBinary("1.2.3-ubuntu-amd64")
 		v := doc{&bp}
 		data, err := m.marshal(&v)
 		c.Assert(err, jc.ErrorIsNil)
